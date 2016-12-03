c9353dd4c8cc924910c0df492eba7add9441e634
Alex <dovzhanyn.alex@gmail.com>
2016-12-03 16:28:32 -0500

    womp

--- viewGen.js
+++ viewGen.js
@@ -1,5 +1,5 @@
 function generateView() {
-  return "<div style='width: 400px; position: fixed; z-index: 100000; bottom: 0; margin: 0; right: 20px; background-color: #3b4773; border-top-right-radius: 5px; border-top-left-radius: 5px; font-family: sans-serif; color: #fff; text-align: center; padding-top: .5%; padding-bottom: .5%; box-shadow: 0px 2px 0px #990000;'><a href='javascript:void(0);' style='display: inline-block; margin: 0%; padding: 0; color: #fff;' id='expandLinks'>View Window.href</a><div style='display:none' id='hiddenLinks'></div></div>";
+  return "<div style='width: 400px; position: fixed; z-index: 100000; bottom: 0; margin: 0; right: 20px; background-color: #3b4773; border-top-right-radius: 5px; border-top-left-radius: 5px; font-family: sans-serif; color: #fff; text-align: center; padding-top: .5%; padding-bottom: .5%; box-shadow: 0px 2px 0px #990000;'><p style='display: inline-block; margin: 0%; padding: 0; color: #fff;' id='expandLinks'>View Window.href</p><div style='display:none' id='hiddenLinks'>hiiii</div></div>";
 }
 
 function appendView() {
@@ -8,7 +8,9 @@ function appendView() {
 }
 
 $(document).ready(function(){
-	$('#expandLinks').click(function(){
+	appendView();
 
+	$('#expandLinks').on('click', function(){
+		$('#hiddenLinks').css({"display": "inline-block"});
 	})
 })
