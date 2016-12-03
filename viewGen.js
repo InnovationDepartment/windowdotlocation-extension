ed0c698e59eee3a5788392c06f2d819f992ce7fb
Alex <dovzhanyn.alex@gmail.com>
2016-12-03 17:45:19 -0500

    updates instead of listing

--- viewGen.js
+++ viewGen.js
@@ -1,5 +1,5 @@
 function generateView() {
-  var appendable = "<div style='width: 400px; position: fixed; z-index: 100000; bottom: 0; margin: 0; right: 20px; background-color: #3b4773; border-top-right-radius: 5px; border-top-left-radius: 5px; font-family: sans-serif; color: #fff; text-align: center; box-shadow: 0px 2px 0px #990000;'><p style='display: inline-block; margin: 0%; padding: 0; color: #fff; width: 100%; border-bottom: 1px solid rgba(255,255,255,0.4); padding: 10px 0px; cursor: pointer;' id='expandLinks'>View Window.href <span id='caret'>&darr;</span></p><div style='display:none; background: #fff; border: 1px solid rgba(0,0,0,0.4);' id='hiddenLinks'>";
+  var appendable = "<div style='width: 400px; position: fixed; z-index: 100000; bottom: 0; margin: 0; right: 20px; background-color: #3b4773; border-top-right-radius: 5px; border-top-left-radius: 5px; font-family: sans-serif; color: #fff; text-align: center; box-shadow: 0px 2px 0px #990000;'><p style='display: inline-block; margin: 0%; padding: 0; color: #fff; width: 100%; border-bottom: 1px solid rgba(255,255,255,0.4); padding: 10px 0px; cursor: pointer;' id='expandLinks'>View Window.href <span id='caret'>&darr;</span></p><div style='display:none; background: #fff; border: 1px solid rgba(0,0,0,0.4); max-height: 400px; overflow-y: scroll;' id='hiddenLinks'>";
   $(window.visits).each(function(index, visit){
   	appendable += "<div style='width: 100%; color: rgba(0,0,0,0.8); padding: 10px 0px; border-bottom: 1px solid rgba(0,0,0,0.4);'>" + visit.username + ": " + "<a href='" + visit.url + "' target='_blank'>" + visit.url + "</a></div>"
   });
@@ -28,5 +28,9 @@ function initializeView() {
 }
 
 function updateView(visit) {
-	$('#hiddenLinks').append("<div style='width: 100%; color: rgba(0,0,0,0.8); padding: 10px 0px; border-bottom: 1px solid rgba(0,0,0,0.4);'>" + visit.username + ": " + visit.url + "</div>");
+	if ( $("#hiddenLinks #" + visit.username).length > 0 ){
+		$("#hiddenLinks #" + visit.username).html(visit.username + ": " + "<a href='" + visit.url + "' target='_blank'>" + visit.url + "</a>");
+	} else {
+		$('#hiddenLinks').append("<div style='width: 100%; color: rgba(0,0,0,0.8); padding: 10px 0px; border-bottom: 1px solid rgba(0,0,0,0.4);' id='" + visit.username + "'>" + visit.username + ": " + "<a href='" + visit.url + "' target='_blank'>" + visit.url + "</a></div>");
+	}
 }
